53d471e319acca649445e57ab8ddb184c9e009bd
Marta <martaf@cyberia.pl>
2019-10-11 11:20:46 +0200

    Add navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { RatesComponent } from './rates/rates.component';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { AlertsComponent } from './alerts/alerts.component';
+import { PlaygroundComponent } from './playground/playground.component';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/rates', pathMatch: 'full' },
     { path: 'rates', component: RatesComponent },
     { path: 'calculator', component: CalculatorComponent },
     { path: 'alerts', component: AlertsComponent },
+    { path: 'playground', component: PlaygroundComponent },
     { path: '**', redirectTo: '/rates' },
 ];
 
