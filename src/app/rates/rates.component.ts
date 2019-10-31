35e8bd9de38d33ac472a35707571f0c2145f3d8b
Marta Fiedorowicz <martaf@cyberia.pl>
2019-10-31 21:19:27 +0100

    Chart takes data from service without errors

--- src/app/rates/rates.component.ts
+++ src/app/rates/rates.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ChartType, ChartDataSets, ChartOptions } from 'chart.js';
-import { MultiDataSet, Label, Color } from 'ng2-charts';
+import { MultiDataSet, Label, Color, BaseChartDirective } from 'ng2-charts';
 import { NBPService } from '../services/nbp.service';
 
 @Component({
@@ -11,18 +11,36 @@ import { NBPService } from '../services/nbp.service';
 export class RatesComponent implements OnInit {
 
   public lineChartData: ChartDataSets[] = [
-    { data: this.rateService.getExchangeRates(1, 1, 1).values, label: 'Series A' }
+    { data: [1], label: 'SeriesA' }
   ];
-  public lineChartLabels: Label[] = this.rateService.getExchangeRates(1, 1, 1).dates;
-  public lineChartOptions: (ChartOptions & { annotation: any });
-  public lineChartColors: Color[];
+  public lineChartLabels: Label[] = ['L1'];
+  public lineChartOptions: (ChartOptions & { annotation: any }) = {
+    annotation: {},
+  };
+  public lineChartColors: Color[] = [
+    { 
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    }
+  ];
   public lineChartLegend = false;
   public lineChartType = 'line';
   public lineChartPlugins = [];
 
-  constructor(private rateService: NBPService) { }
+  @ViewChild(BaseChartDirective, { static: true }) chart: BaseChartDirective;
+
+  constructor(private rateService: NBPService) {
+  }
 
   ngOnInit() {
+    let exchangeRates = this.rateService.getExchangeRates('USD',1,1);
+    this.lineChartData[0].data = exchangeRates.values;
+    this.lineChartLabels = exchangeRates.dates;
+    this.chart.update();
   }
 
 }
