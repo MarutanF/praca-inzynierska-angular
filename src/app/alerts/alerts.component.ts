567240a0cabcb910f18361fc6a07cfe3abb3f07c
Marta Fiedorowicz <martaf@cyberia.pl>
2019-11-13 14:52:55 +0100

    Basic alerts

--- src/app/alerts/alerts.component.ts
+++ src/app/alerts/alerts.component.ts
@@ -1,21 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseOptimalAlertsService, OptimalAlert } from '../services/firebase-optimal-alerts.service';
-import { ChildActivationStart } from '@angular/router';
+import { Currency, NBPCurrenciesService } from '../services/nbp-currencies.service';
 
 @Component({
   selector: 'app-alerts',
   templateUrl: './alerts.component.html',
   styleUrls: ['./alerts.component.css']
 })
+
 export class AlertsComponent implements OnInit {
 
+  // ALERTS
   public alert$;
 
-  constructor(private optimalAlertService: FirebaseOptimalAlertsService) {
+  // CURRENCIES DROPDOWN
+  public listOfCurrencies: Currency[] = [];
+  public selectedCurrency: Currency;
+
+  // DATAPICKER
+  public selectedData;
+
+  constructor(
+    private optimalAlertService: FirebaseOptimalAlertsService,
+    private currenciesService: NBPCurrenciesService) {
     this.alert$ = this.optimalAlertService.getUserAlerts();
   }
 
-  ngOnInit() {
+  async ngOnInit() {
+    await this.initializeCurrenciesDropdown();
+  }
+
+  async initializeCurrenciesDropdown() {
+    this.listOfCurrencies = await this.currenciesService.getCurrenciesList();
+    this.selectedCurrency = this.listOfCurrencies[0];
+  }
+
+  deleteAlert(alert) {
+    this.optimalAlertService.deleteAlert(alert);
   }
 
   myFun(data) {
