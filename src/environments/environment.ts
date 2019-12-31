1fd51722c8c20a08792c5ce8ec203e2afde8f09d
Marta Fiedorowicz <martaf@cyberia.pl>
2019-12-31 17:55:09 +0100

    Add alt parameter to icon and new env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAzg1hLTV8njFDx14Pe2wgJ_AQRaFygjbU",
-    authDomain: "praca-inzynierska-firebase.firebaseapp.com",
-    databaseURL: "https://praca-inzynierska-firebase.firebaseio.com",
-    projectId: "praca-inzynierska-firebase",
-    storageBucket: "praca-inzynierska-firebase.appspot.com",
-    messagingSenderId: "941510561750",
-    appId: "1:941510561750:web:e9c59d2bf7083f32be9e77",
-    measurementId: "G-EH4Z1TY96G"
+    apiKey: 'AIzaSyAzg1hLTV8njFDx14Pe2wgJ_AQRaFygjbU',
+    authDomain: 'praca-inzynierska-firebase.firebaseapp.com',
+    databaseURL: 'https://praca-inzynierska-firebase.firebaseio.com',
+    projectId: 'praca-inzynierska-firebase',
+    storageBucket: 'praca-inzynierska-firebase.appspot.com',
+    messagingSenderId: '941510561750',
+    appId: '1:941510561750:web:e9c59d2bf7083f32be9e77',
+    measurementId: 'G-EH4Z1TY96G'
   }
 };
 
